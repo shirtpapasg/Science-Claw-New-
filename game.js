53362a66238ca2f2bba36aac9fb0eb28b92d3e0f
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 12:48:17 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,118 @@
+let clawX = 300;
 
-function playMachine(){
+let dropping = false;
+
+let prizes = [];
+
+const GAME_WIDTH = 800;
+
+const GAME_HEIGHT = 500;
+
+function initGame(){
+
+const container =
+
+document.getElementById(
+"canvas-container"
+);
+
+container.innerHTML =
+
+`
+
+<canvas
+id="gameCanvas"
+width="800"
+height="500">
+</canvas>
+
+`;
+
+const canvas =
+
+document.getElementById(
+"gameCanvas"
+);
+
+const ctx =
+
+canvas.getContext("2d");
+
+for(
+
+let i=0;
+i<10;
+i++
+
+){
+
+prizes.push({
+
+x:
+
+Math.random()*700+40,
+
+y:
+
+350+
+
+Math.random()*80,
+
+caught:false
+
+});
+
+}
+
+document.addEventListener(
+
+"keydown",
+
+handleControls
+
+);
+
+loop(ctx);
+
+}
+
+function handleControls(e){
+
+if(dropping) return;
+
+if(
+
+e.key==="ArrowLeft"
+
+){
+
+clawX -= 25;
+
+}
+
+if(
+
+e.key==="ArrowRight"
+
+){
+
+clawX += 25;
+
+}
+
+if(
+
+e.key===" "
+
+){
+
+dropClaw();
+
+}
+
+}
+
+function dropClaw(){
 
 if(
 
@@ -8,9 +121,7 @@ player.credits<=0
 ){
 
 alert(
-
-"No credits left"
-
+"No credits"
 );
 
 return;
@@ -19,36 +130,93 @@ return;
 
 player.credits--;
 
-const success =
-
-Math.random();
-
-if(
-
-success > 0.65
-
-){
-
-player.prizes++;
-
-alert(
-
-"You won a prize!"
-
-);
-
-}else{
-
-alert(
-
-"Missed!"
-
-);
-
-}
-
 updateHUD();
 
 saveLocal();
+
+dropping = true;
+
+}
+
+function loop(ctx){
+
+requestAnimationFrame(
+()=>loop(ctx)
+);
+
+ctx.clearRect(
+
+0,
+0,
+
+GAME_WIDTH,
+GAME_HEIGHT
+
+);
+
+ctx.fillStyle="white";
+
+ctx.fillRect(
+
+clawX,
+0,
+
+10,
+150
+
+);
+
+ctx.fillRect(
+
+clawX-20,
+150,
+
+50,
+20
+
+);
+
+for(
+
+const p of prizes
+
+){
+
+if(
+p.caught
+) continue;
+
+ctx.beginPath();
+
+ctx.arc(
+
+p.x,
+p.y,
+
+18,
+
+0,
+
+Math.PI*2
+
+);
+
+ctx.fill();
+
+}
+
+if(
+
+dropping
+
+){
+
+const claw =
+
+document.querySelector(
+"#gameCanvas"
+);
+
+}
 
 }
