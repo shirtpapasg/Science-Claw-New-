d8a621d910f36eb73ffa90e03c3e4f483f22ff4a
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 12:43:09 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,51 @@
+function showTopUpMenu(){
 
+document
+.getElementById(
+"topup-modal"
+)
+.style.display="block";
+
+}
+
+function startQuiz(
+
+questions,
+
+reward
+
+){
+
+document
+.getElementById(
+"topup-modal"
+)
+.style.display="none";
+
+const pass =
+
+confirm(
+
+`Pretend quiz complete?\nReward: ${reward}`
+
+);
+
+if(!pass) return;
+
+player.credits += reward;
+
+player.questionsAttempted += questions;
+
+player.questionsCorrect += questions;
+
+updateHUD();
+
+saveLocal();
+
+alert(
+
+`+${reward} credits`
+
+);
+
+}
