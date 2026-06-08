f23f469bcbc9295d707c37106cf5a843f2fac20f
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 08:07:46 +0800

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,1 +1,105 @@
-backend.js
+let player = {
+
+name:"",
+
+studentClass:"",
+
+credits:0,
+
+streak:0,
+
+prizes:0,
+
+questionsCorrect:0,
+
+questionsAttempted:0
+
+};
+
+function authorizeStudentLogin(){
+
+player.name =
+
+document.getElementById(
+"student-name"
+).value;
+
+player.studentClass =
+
+document.getElementById(
+"student-class"
+).value;
+
+if(
+
+player.name.trim()===""
+
+){
+
+alert(
+
+"Enter your name"
+
+);
+
+return;
+
+}
+
+player.credits = 3;
+
+document
+.getElementById(
+"registration-screen"
+)
+.style.display =
+"none";
+
+updateHUD();
+
+saveLocal();
+
+}
+
+function saveLocal(){
+
+localStorage.setItem(
+
+"sciencePlayer",
+
+JSON.stringify(player)
+
+);
+
+}
+
+function loadLocal(){
+
+const data =
+
+localStorage.getItem(
+"sciencePlayer"
+);
+
+if(!data) return;
+
+player =
+
+JSON.parse(data);
+
+document
+.getElementById(
+"registration-screen"
+)
+.style.display =
+"none";
+
+updateHUD();
+
+}
+
+window.onload=()=>{
+
+loadLocal();
+
+};
