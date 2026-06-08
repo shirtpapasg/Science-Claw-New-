6a3c1fba09f6840203f12f6c7428b00057ea5df7
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 12:42:49 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,54 @@
 
+function playMachine(){
+
+if(
+
+player.credits<=0
+
+){
+
+alert(
+
+"No credits left"
+
+);
+
+return;
+
+}
+
+player.credits--;
+
+const success =
+
+Math.random();
+
+if(
+
+success > 0.65
+
+){
+
+player.prizes++;
+
+alert(
+
+"You won a prize!"
+
+);
+
+}else{
+
+alert(
+
+"Missed!"
+
+);
+
+}
+
+updateHUD();
+
+saveLocal();
+
+}
