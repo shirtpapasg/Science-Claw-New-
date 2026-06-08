98c2403c7a2f4d496765dde3c6804090a8b5d835
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 14:51:29 +0800

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,32 +4,28 @@ document
 .getElementById(
 "player-hud-tag"
 )
-.innerText =
-
+innerText =
 player.name;
 
 document
 .getElementById(
 "credit-display"
 )
-.innerText =
-
+innerText =
 player.credits;
 
 document
 .getElementById(
 "streak-display"
 )
-.innerText =
-
+innerText =
 player.streak;
 
 document
 .getElementById(
 "prizes-display"
 )
-.innerText =
-
+innerText =
 player.prizes;
 
 let accuracy = 0;
@@ -40,9 +36,7 @@ player.questionsAttempted > 0
 
 ){
 
-accuracy =
-
-Math.round(
+accuracy = Math.round(
 
 player.questionsCorrect /
 
@@ -59,7 +53,86 @@ document
 "accuracy-display"
 )
 .innerText =
-
 accuracy + "%";
+
+renderInstructions();
+
+}
+
+
+
+function renderInstructions(){
+
+let box =
+
+document.getElementById(
+"instructions-box"
+);
+
+if(
+
+!box
+
+){
+
+box =
+
+document.createElement(
+"div"
+);
+
+box.id =
+"instructions-box";
+
+box.style.position =
+"fixed";
+
+box.style.right =
+"20px";
+
+box.style.bottom =
+"20px";
+
+box.style.padding =
+"12px";
+
+box.style.background =
+"rgba(0,0,0,.8)";
+
+box.style.color =
+"white";
+
+box.style.border =
+"1px solid white";
+
+document.body.appendChild(
+box
+);
+
+}
+
+box.innerHTML =
+
+`
+
+<b>Controls</b>
+
+<br><br>
+
+⬅️ ➡️ Move Claw
+
+<br>
+
+SPACE = Drop Claw
+
+<br><br>
+
+🎯 Catch prizes
+
+<br>
+
+🧠 Earn credits via quiz
+
+`;
 
 }
