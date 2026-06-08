23490d1df8b3a039ef4b9a0c36aceeb4e95744cb
shirtpapasg <shirtpapasg@gmail.com>
2026-06-08 08:08:02 +0800

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,65 @@
+function updateHUD(){
 
+document
+.getElementById(
+"player-hud-tag"
+)
+.innerText =
+
+player.name;
+
+document
+.getElementById(
+"credit-display"
+)
+.innerText =
+
+player.credits;
+
+document
+.getElementById(
+"streak-display"
+)
+.innerText =
+
+player.streak;
+
+document
+.getElementById(
+"prizes-display"
+)
+.innerText =
+
+player.prizes;
+
+let accuracy = 0;
+
+if(
+
+player.questionsAttempted > 0
+
+){
+
+accuracy =
+
+Math.round(
+
+player.questionsCorrect /
+
+player.questionsAttempted
+
+*100
+
+);
+
+}
+
+document
+.getElementById(
+"accuracy-display"
+)
+.innerText =
+
+accuracy + "%";
+
+}
